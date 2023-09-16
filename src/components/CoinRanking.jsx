83d90749da51c78c20581d702978324133ebbfc7
belfarz <115107231+belfarz@users.noreply.github.com>
2023-09-16 07:21:32 +0100

    Your commit message here

--- src/components/CoinRanking.jsx
+++ src/components/CoinRanking.jsx
@@ -7,7 +7,7 @@ import axios from 'axios'
 export default function CoinRanking() {
 
     const [coin, setCoin] = useState([])
-    const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C7d%2C24h&locale=en"
+    const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C7d%2C24h&locale=en"
 
     useEffect(()=>{
         axios.get(url).then((response)=>{
