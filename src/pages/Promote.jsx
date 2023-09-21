8dd38dc61ae8a4d390929c142a9b6d94cf9ddba7
belfarz <115107231+belfarz@users.noreply.github.com>
2023-09-21 17:35:35 +0100

    promote coin page

--- src/pages/Promote.jsx
+++ src/pages/Promote.jsx
@@ -2,8 +2,65 @@ import React from 'react'
 
 export default function Promote() {
   return (
-    <div className='mt-64 bg-white'>
-      <h1>this is the promote page</h1>
+    <div>
+    <h1 className="ppp">Promote Packages Prices</h1>
+    <div className="flex flex-wrap justify-center">
+        <div className="col-4">
+            <div className="checkbox">
+                <input type="radio" name="options" id="option1"/>$130 - 1 DAY <br/><br/>
+                <input type="radio" name="options" id="option2"/> $630 - 7 DAYS  <br/><br/>
+                <input type="radio" name="options" id="option3"/> $630 - 7 DAYS <br/><br/>
+                <button className="buttonck" onclick="redirectToTelegram()">BOOK YOUR AD</button>
+            </div>
+        </div>
+
+        <div className="col-4">
+            <div className="checkbox">
+                <input type="radio" name="options" id="option7"/>$130 - 1 DAY <br/><br/>
+                <input type="radio" name="options" id="option8"/> $630 - 7 DAYS<br/><br/>
+                <input type="radio" name="options" id="option9"/> $630 - 7 DAYS  <br/><br/>
+                <button className="buttonck" onclick="redirectToTelegram2()">BOOK YOUR AD</button>
+            </div>
+        </div>
+
+        <div className="col-4">
+            <div className="checkbox">
+                <input type="radio" name="options" id="option10"/>$130 - 1 DAY <br/><br/>
+                <input type="radio" name="options" id="option11"/> $630 - 7 DAYS  <br/><br/>
+                <input type="radio" name="options" id="option12"/> $630 - 7 DAYS <br/><br/>
+                <button className="buttonck" onclick="redirectToTelegram4()">BOOK YOUR AD</button>
+            </div>
+        </div>
+
+        <div className="col-4">
+            <div className="checkbox">
+                <input type="radio" name="options" id="option13"/>$130 - 1 DAY <br/><br/>
+                <input type="radio" name="options" id="option14"/> $630 - 7 DAYS<br/><br/>
+                <input type="radio" name="options" id="option15"/> $630 - 7 DAYS<br/><br/>
+                <button className="buttonck" onclick="redirectToTelegram3()">BOOK YOUR AD</button>
+            </div>
+        </div>
+
+        <div className="col-4">
+            <div className="checkbox">
+                <input type="radio" name="options" id="option16"/>$130 - 1 DAY<br/><br/>
+                <input type="radio" name="options" id="option17"/> $630 - 7 DAYS<br/><br/>
+                <input type="radio" name="options" id="option18"/> $630 - 7 DAYS <br/><br/>
+                <button className="buttonck" onclick="redirectToTelegram2()">BOOK YOUR AD</button>
+            </div>
+        </div>
+
+        <div className="col-4">
+            <div className="checkbox">
+                <input type="radio" name="options" id="option4"/>$130 - 1 DAY<br/><br/>
+                <input type="radio" name="options" id="option5"/> $630 - 7 DAYS<br/><br/>
+                <input type="radio" name="options" id="option6"/> $630 - 7 DAYS <br/><br/>
+                <button className="buttonck" onclick="redirectToTelegram1()">BOOK YOUR AD</button>
+            </div>
+        </div>
     </div>
+    </div>
+
+
   )
 }
