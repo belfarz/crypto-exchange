1cd8ab22f1d0b5b86a853a2dc2cfa9d834273912
belfarz <115107231+belfarz@users.noreply.github.com>
2023-10-07 18:45:35 +0100

    update styles and added presale

--- src/components/CoinAds.jsx
+++ src/components/CoinAds.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import add1 from "../image/ad-1.jpg"
+import add2 from "../image/add2.jpg"
+import add3 from "../image/add3.jpg"
+import add4 from "../image/add4.jpg"
 
 export default function CoinAds() {
   return (
     <div className='flex flex-col justify-center mt-2 mb-2 lg:ml-64'>
       <div className='flex justify-center'>
-        <div className=' w-full lg:w-[1022px] h-[115px] bg-white flex justify-center items-center m-2'id='ad-1' > </div>
+        <div className=' w-full lg:w-[1022px]  bg-white flex justify-center items-center m-2'id='ad-1' > <img src={add1} alt="" /></div>
       </div>
       <div className='flex justify-center  overflow-x-auto'>
-        <div className='w-full md:w-[600px]  h-[240px] bg-white flex justify-center items-center m-2 ' ><h1>Left ADVERT</h1> </div>
-        <div className=' md:block w-[600px] hidden h-[240px] bg-white lg:flex justify-center items-center m-2 ' ><h1>Middle ADVERT</h1> </div>
-        <div className=' md:block w-[600px] hidden  h-[240px] bg-white lg:flex justify-center items-center m-2 ' ><h1> ADVERT</h1> </div>
+        <div className='w-full md:w-[600px]  bg-black flex justify-center items-center m-2 ' > <img src={add2} alt="" /></div>
+        <div className=' md:block w-[600px] hidden bg-black lg:flex justify-center items-center m-2 ' > <img src={add3} alt="" /> </div>
+        <div className=' md:block w-[600px] hidden  bg-black lg:flex justify-center items-center m-2 ' > <img src={add4} alt="" /></div>
       </div>
     </div>
   )
